92bffc72728a6984e8f571ce6985afb2aa6cd97a
Marisa Li <marisa.susan.li@gmail.com>
2020-08-13 15:45:47 -0400

    added Page and User classes

--- app.js
+++ app.js
@@ -3,11 +3,29 @@ const app = express();
 const morgan = require('morgan');
 const layout = require('./views/layout.js');
 
+// const { db } = require('./models');
+const { db, Page, User } = require('./models');
+
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res, send) => res.send(layout('')));
 
-const port = 3000;
-app.listen(port, () => console.log('port is working'));
+db.authenticate().then(() => {
+  console.log('connected to the database');
+});
+
+const PORT = 3000;
+
+const init = async () => {
+  await db.sync({ force: true });
+
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}!`);
+  });
+};
+
+init();
+
+// app.listen(port, () => console.log('port is working'));
