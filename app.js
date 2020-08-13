cfe529a88de273049f5d4ea8399abe35a45734cf
Marisa Li <marisa.susan.li@gmail.com>
2020-08-13 13:03:49 -0400

    test commit to make sure this works

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-const express = require ('express')
-const app = express()
-const morgan = require('morgan')
-const layout = require('./views/layout.js')
-app.use(morgan('dev'))
-app.use(express.static(__dirname+'/public'))
-app.use(express.urlencoded({ extended: false }));
-app.get('/', (req, res, send) => res.send(layout('')))
-const port = 3000
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const layout = require('./views/layout.js');
 
-app.listen(port, () => console.log('port is working'))
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res, send) => res.send(layout('')));
+
+const port = 3000;
+app.listen(port, () => console.log('port is working'));
